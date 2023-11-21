9f642368dadaa60a05d36bbbff70250b39e2935b
HTML Project <ksiushabliz@gmail.com>
2023-11-21 20:17:35 +0200

    HomeWork1.js

---
+++ Lesson1/Task1.js
@@ -0,0 +1,15 @@
+let a1= 'hello';
+let b1= 'owu';
+let c1= 'com';
+let d1= 'ua';
+let e1= 1;
+let f1= 10;
+let g1= -999;
+let h1= 123;
+let i1= 3.14;
+let j1= 2.7;
+let k1= 16;
+let l1= true;
+let m1= false;
+
+console.log(a1,b1,c1,d1,e1,f1,g1,h1,i1,j1,k1,l1,m1);
